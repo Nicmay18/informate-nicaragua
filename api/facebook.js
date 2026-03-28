3d72dfb04493e5ef8f538867685dc9e7385555d6
Jose Noel Diaz <maycolnicaragua2018@gmail.com>
2026-03-27 18:06:51 -0600

    Actualizar admin con APIs de Telegram y Facebook

--- api/facebook.js
+++ api/facebook.js
@@ -1,5 +1,5 @@
 // api/facebook.js
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   if (req.method === 'OPTIONS') return res.status(200).end();

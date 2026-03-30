e691ae685f280b445631a0ea19c373560a2796a3
Jose Noel Diaz <maycolnicaragua2018@gmail.com>
2026-03-30 14:59:12 -0600

    pwa-schema-seo

---
+++ public/sw.js
@@ -0,0 +1,40 @@
+const CACHE = 'ni-v1';
+const STATIC = [
+  '/',
+  '/index.html',
+  '/noticia.html',
+  '/logo.png',
+  '/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(STATIC)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  const url = new URL(e.request.url);
+  // Solo cachear mismo origen
+  if (url.origin !== location.origin) return;
+
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      const network = fetch(e.request).then(res => {
+        if (res.ok) {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      });
+      return cached || network;
+    })
+  );
+});

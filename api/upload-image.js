91c4c58fe167cb3895a6865a7d3affa9d28c6627
Jose Noel Diaz <maycolnicaragua2018@gmail.com>
2026-03-30 12:27:15 -0600

    subir-imagen-github-url

--- api/upload-image.js
+++ api/upload-image.js
@@ -1,4 +1,4 @@
-module.exports = async (req, res) => {
+export default async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   if (req.method === 'OPTIONS') return res.status(200).end();
